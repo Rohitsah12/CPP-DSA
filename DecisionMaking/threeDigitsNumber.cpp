8a577c1df3cbbe20ef2c73e643b077d5c016ce71
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-21 16:57:29 +0530

    ThreeDigitS yes or No

---
+++ DecisionMaking/threeDigitsNumber.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n>=100 && n<1000){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
