a020ad06bd3e00cdcefb3078010503d3838e66c5
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-22 22:32:38 +0530

    practice

---
+++ Ternary and switch/calculators.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    char ch;
+    cin>>a>>ch>>b;
+    switch(ch){
+        case '+': cout<<a+b;
+        break;
+        case '-': cout<<a-b;
+        break;
+        case '*': cout<<a*b;
+        break;
+        case '/': cout<<a/b;
+        break;
+        default:
+        cout<<"Invalid operand";
+
+    }
+}
