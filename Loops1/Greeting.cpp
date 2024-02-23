c031feb3d03788dd04b08213167fdb332da682f8
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-23 18:37:53 +0530

    Starting with loops

---
+++ Loops1/Greeting.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //10 times good morning
+    //automation/repitition
+    for(int i=1;i<=3;i++){
+        cout<<"Good Morning\n";
+    }
+}
