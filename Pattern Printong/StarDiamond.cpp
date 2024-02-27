10b56e4b31bc42f9443c856eb59bbc146558e661
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-27 18:02:41 +0530

    Practice

---
+++ Pattern Printong/StarDiamond.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int nst=1;
+    int nsp=n-1;
+    for(int i=1;i<=2*n-1;i++){
+        for(int i=1;i>=)
+        cout<<endl;
+    }
+
+}
