121f2479ad20f571e55c272aaa20079706750ae8
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-10 19:12:18 +0530

    basic

---
+++ FUNCTIONS/basicfunc.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+void greeting(){
+    cout<<"Good Morning"<<endl;
+    cout<<"Have  nice day";
+}
+int main(){
+    greeting(); //Function calling
+    greeting();
+    greeting();
+}
