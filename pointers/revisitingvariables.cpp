25e6bbec9d3b312dc522f48597a5615b04686c89
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-11 17:20:52 +0530

    pointers

--- pointers/revisitingvariables.cpp
+++ pointers/revisitingvariables.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<sizeof(int);
+    // cout<<sizeof(int);
+    // cout<<sizeof(char);
+    // cout<<sizeof(bool);
+    // cout<<sizeof(float);
+    int x=5;
+    cout<<&x;
 }
