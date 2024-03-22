084e35d347eae715e5b9a76c8c72040dabb8c943
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-22 15:17:59 +0530

    arithmetic in pointers

--- pointers/arithemetic.cpp
+++ pointers/arithemetic.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int x=7;
+    // int x=7;
+    // int* ptr=&x;
+    // cout<<ptr<<endl;
+    // ptr=ptr+1;
+    // cout<<ptr<<endl;
+    int x=4;
     int* ptr=&x;
-    cout<<ptr<<endl;
-    ptr=ptr+1;
-    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    *ptr=*ptr+1;
+    cout<<*ptr<<endl;
 }
