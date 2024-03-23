1ff915ee9d78406f3d40243c8c952ef0ba157beb
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-23 21:08:26 +0530

    Taking input in vectors

--- array2/vectorsbasic.cpp
+++ array2/vectorsbasic.cpp
@@ -4,9 +4,13 @@ int main(){
     vector<int> v;//you need not mention size
     //inserting/input donot use square bracket[]
     v.push_back(6);
+    cout<<v.capacity()<<endl;
     v.push_back(1);
+    cout<<v.capacity()<<endl;
     v.push_back(9);
+     cout<<v.capacity()<<endl;
     v.push_back(0);
+     cout<<v.capacity()<<endl;
     //if you want to update/access use [] brackets
     cout<<v[0]<<" ";
     cout<<v[1]<<" ";
