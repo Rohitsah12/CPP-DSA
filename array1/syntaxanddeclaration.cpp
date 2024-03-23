6230538705d84888c3e53237bc6236d3153cdf59
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-23 07:58:37 +0530

    starting with array

---
+++ array1/syntaxanddeclaration.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[7]={1,5,9,4,6,3,1};//0 1 2 3 4 5 6
+    // arr[0]=7;
+    // arr[1]=1;
+    // arr[2]=6;
+    // arr[3]=3;
+    // arr[4]=9;
+    // arr[5]=7;
+    // arr[6]=0;
+    cout<<arr[0];
+}
