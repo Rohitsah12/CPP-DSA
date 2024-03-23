3c8cc29404d3a177e6798b5f5b95ca40656662f3
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-23 20:06:09 +0530

    starting with vectors

---
+++ array2/vectorsbasic.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;//you need not mention size
+    //inserting/input donot use square bracket[]
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(0);
+    //if you want to update/access use [] brackets
+    cout<<v[0]<<" ";
+    cout<<v[1]<<" ";
+    cout<<v[2]<<" ";
+    cout<<v[3]<<" ";
+}
