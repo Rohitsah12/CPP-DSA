aae0356ae684512411dfdd9f0f00d3ecd78de730
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-23 08:17:55 +0530

    marks less than 35

---
+++ array1/markslessthan35.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int marks[6];
+    for(int i=0 ; i<6;i++){
+        cin>>marks[i];
+    }
+    for(int i=0;i<6;i++){
+        if(marks[i]<35){
+            cout<<i<<" ";
+        }
+    }
+}
--- array1/syntaxanddeclaration.cpp
+++ array1/syntaxanddeclaration.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[7]={1,5,9,4,6,3,1};//0 1 2 3 4 5 6
-    // arr[0]=7;
-    // arr[1]=1;
-    // arr[2]=6;
-    // arr[3]=3;
-    // arr[4]=9;
-    // arr[5]=7;
-    // arr[6]=0;
-    cout<<arr[0];
+    int arr[7];
+    //input 
+    for(int i=0;i<6;i++){
+        cin>>arr[i];
+    }
+    //output
+    for(int i=0;i<6;i++){
+        cout<<arr[i];
+    }
 }
