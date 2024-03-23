3a92e3f8b3bf2085b80c87f5272d82a419fbd412
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-23 09:18:11 +0530

    2nd max in array

---
+++ array1/2ndmaxinarray.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int max=INT_MIN;
+    for(int i=0 ;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++){
+        if(max<arr[i]) max=arr[i];
+    }
+    int smax=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=max && smax<arr[i]) smax=arr[i];
+    }
+    cout<<max<<endl<<smax;
+}
