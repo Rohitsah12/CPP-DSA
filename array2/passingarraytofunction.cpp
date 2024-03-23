de356cc1f5ad21cd998e1823f8aec7fc408116a2
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-23 18:41:49 +0530

    passing array to a function

---
+++ array2/passingarraytofunction.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display(int a[]){
+    for(int i=0;i<=4;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    return;
+}
+void change(int b[]){
+    b[0]=100;
+}
+int main(){
+    int arr[]={1,4,2,7,4};
+    //accessing the elements of array in another function
+    //updation, pass by value/reference 
+    display(arr);
+    change(arr);
+    display(arr);
+}
