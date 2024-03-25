3eba606986241dc073b882aa895cac74e940151d
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-25 11:01:06 +0530

    2darrays

---
+++ 2darrays1/secondlargest.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int max=INT_MIN;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+           if(max<arr[i][j]){
+            max=arr[i][j];
+           }
+        }
+    }
+    cout<<max;
+}
