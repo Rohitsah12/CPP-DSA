034fa7bb237d96643060e42113ef9617cf374237
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-28 11:10:54 +0530

    longest common prefix

--- string2/longestcommonprefix.cpp
+++ string2/longestcommonprefix.cpp
@@ -1,17 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    vector<string> v;//["flower","flow","flight"]
-    v.push_back("flower");
-    v.push_back("flow");
-    v.push_back("flight");
-    //lexographically
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<endl;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        if(n==1) return strs[0];
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last=strs[n-1];
+        string s="";
+        for(int i=0;i<min(first.length(),last.length());i++){
+            if(first[i]==last[i]){
+                s+=first[i];
+            }
+            else return s;
+        }
+        return s;
     }
-    cout<<endl;
-    sort(v.begin(),v.end());
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<endl;
-    }
-}
+};
