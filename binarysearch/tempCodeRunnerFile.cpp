666963217959ac96d00598b9c55ff6c9f2004337
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-02 10:58:15 +0530

    row with maximum one's

--- binarysearch/tempCodeRunnerFile.cpp
+++ binarysearch/tempCodeRunnerFile.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int lastOccurrence(vector<int>& nums, int target) {
     int low = 0;
     int high = nums.size() - 1;
-    bool flag = false;
     while (low <= high) {
         int mid = low + (high - low) / 2;
         if (nums[mid] == target) {
             if (mid == nums.size() - 1 || nums[mid + 1] != target) {
-                flag = true;
                 return mid;
             }
             else {
@@ -21,18 +19,16 @@ int lastOccurrence(vector<int>& nums, int target) {
         else
             high = mid - 1;
     }
-    if (!flag) return -1;
+     return -1;
 }
 
 int firstOccurrence(vector<int>& nums, int target) {
     int low = 0;
     int high = nums.size() - 1;
-    bool flag = false;
     while (low <= high) {
         int mid = low + (high - low) / 2;
         if (nums[mid] == target) {
             if (mid == 0 || nums[mid - 1] != target) {
-                flag = true;
                 return mid;
             }
             else {
@@ -44,14 +40,22 @@ int firstOccurrence(vector<int>& nums, int target) {
         else
             high = mid - 1;
     }
-    if (!flag) return -1;
+     return -1;
 }
 
 int main() {
-    vector<int> v = {0,0,0,0,1,1};
+    vector<vector<int>> v = {{0,1,1,1},{0,0,1,1},{1,1,1,1},{0,0,0,0}};
     int target = 1;
-    int endIndex = lastOccurrence(v, target);
-    int firstIndex = firstOccurrence(v, target);
-    cout << endIndex - firstIndex + 1;
+    int maxcount = 0;
+    for(int i = 0; i < v.size(); i++) {
+        int left = lastOccurrence(v[i], target);
+        if(left != -1) {
+            int right = firstOccurrence(v[i], target);
+            int count = (right - left) + 1;
+            if(maxcount < count) maxcount = count;
+        }
+    }
+    cout << maxcount;
+
     return 0;
 }
