5a8a143078e102e2d092c2ec66f6f88d9304f2ac
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-15 23:18:40 +0530

    modular exponentiation

--- tle/poweralgo.cpp
+++ tle/poweralgo.cpp
@@ -1,6 +1,8 @@
+const int MOD=10000000007
 int power(int a,int b){
     if(b==0) return 1;
+    a%=MOD;
     int temp=power(a,b/2);
-    if(a%2==0) return temp*temp;
-    else return temp*temp*a;
+    if(a%2==0) return temp*temp*MOD;
+    else return temp*temp*MOD*a*MOD;
 }
