bc94e6fabfa462a944d8bbee6b83c39d27242799
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-16 18:47:47 +0530

    prefix sum update

--- tle/prefixsum.cpp
+++ tle/prefixsum.cpp
@@ -1,13 +1,18 @@
 int prefixsum(vector<int>& a){
     int n=a.size();
     int p[n];
+    // for(int i=0;i<n;i++){
+    //     p[i]=0;
+    //     for(int j=0;j<=i;j++){
+    //         p[i]+=a[j];
+    //     }
+    // }
+    int sum=0;
     for(int i=0;i<n;i++){
-        p[i]=0;
-        for(int j=0;j<=i;j++){
-            p[i]+=a[j];
-        }
+        sum+=a[i];
+        p[i]=sum;
     }
     for(int i=0;i<n;i++){
-        cut<<p[i]<<" ";
+        cout<<p[i]<<" ";
     }
 }
