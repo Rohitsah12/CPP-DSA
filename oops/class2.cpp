59c2d2f9917a13a245169ab2619d6e85576c7313
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-11 13:31:10 +0530

    inline constructor

--- oops/class2.cpp
+++ oops/class2.cpp
@@ -7,9 +7,35 @@ class Customer{
     public:
     //default constructor
     Customer(){
-        cout<<"Hello constructor\n";
+        name="Rohit";
+        account_number=5;
+        balance=100;
+    }
+    //Parameterised Constructor
+    // Customer(string name,int account_number,int balance){
+    //     this->name=name;
+    //     this->account_number=account_number;
+    //     this->balance=balance;
+    // }
+    //constructor overloading
+    Customer(string a,int b){
+        name=a;
+        account_number=b;
+        balance=50;
+    }
+    //Inline constructor
+    inline Customer(string a,int b,int c): name(a),account_number(b),balance(c){
+        
+    }
+    void display(){
+        cout<<name<<" "<<account_number<<" "<<balance<<endl;  
     }
 };
 int main(){
     Customer A1;
+    Customer A2("Rohit",23,1000);
+    Customer A3("Mohit",25);
+    A1.display();
+    A2.display();
+    A3.display();
 }
---
+++ oops/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+Customer(string name,int account_number,int balance){
+    //     this->name=name;
+    //     this->account_number=account_number;
+    //     this->balance=balance;
+    // }
