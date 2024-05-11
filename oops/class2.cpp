d231131d5f22472d0bad8f5294f804f5e666c93c
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-11 12:16:04 +0530

    default constructor

---
+++ oops/class2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Customer{
+    string name;
+    int account_number;
+    int balance;
+    public:
+    //default constructor
+    Customer(){
+        cout<<"Hello constructor\n";
+    }
+};
+int main(){
+    Customer A1;
+}
