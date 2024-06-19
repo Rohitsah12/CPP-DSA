fb98b941fd1f7d4e1a9e662af45d9cd5b83ca0e8
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-19 14:02:00 +0530

    sets

--- sets/basicofset.cpp
+++ sets/basicofset.cpp
@@ -3,4 +3,20 @@
 using namespace std;
 int main(){
     unordered_set<int>s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.erase(2);
+    int target=40;
+    if(s.find(target)!=s.end()){//target exist
+        cout<<"Exist\n";
+    }
+    else cout<<"Doesnot Exist\n";
+    cout<<s.size()<<endl;
+    for(int ele :s){
+        cout<<ele<<" ";
+    }
 }
