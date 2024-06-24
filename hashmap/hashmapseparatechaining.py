82e59cc4612335b103b62d400b63a9f8ba35a97e
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-24 11:12:23 +0530

    insert done

--- hashmap/hashmapseparatechaining.py
+++ hashmap/hashmapseparatechaining.py
@@ -1,3 +1,6 @@
+from curses.ascii import isupper
+
+
 class Entry:
     def __init__(self,key,value):
         self.key=key
@@ -12,8 +15,26 @@ class HT:
 
     def getSize(self):
         return self.size
+
+    '''insert(k,v)
+    1) find index to store this key[0,cap-1]
+    2) check if the key is already present
+    int data[index] ---> present or absent
+    
+    if(present) uodate the value
+    else append it in data[index]'''
+    
     def insert(key,value):
-        
+        entry=Entry(key,value)
+        index=entry.hash % self.capacity
+        isUpdated=False
+        for i in range(self.data[index]) :
+            if(self.data[index][i].key==key):
+                self.data[index][i]=entry
+                isUpdated=True
+                break 
+        if not isUpdated:
+            self.data[index].append(entry)
 
     def remove(key):
 
