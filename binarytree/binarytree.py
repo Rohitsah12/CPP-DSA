b9cd361bad7ccbbe29041a89dc049ab3c256af78
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-01 14:56:24 +0530

    binary tree

---
+++ binarytree/binarytree.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,data,left,right,parent):
+        self.data=data
+        self.left=left
+        self.right=right
+        self.parent=parent
+
+root=Node(1,None,None,None)
+node1=Node(2,None,None,root)
+root.left=node1
+node2=Node(3,None,None,node1)
+node1.left=node2
+node3=Node(4,None,None,node1)
+node1.right=node3
+node4=Node(5,None,None,root)
+root.right=node4
