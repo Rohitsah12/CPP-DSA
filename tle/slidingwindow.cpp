ad6f9457af04a2a0db9939849871eacf5f8c8c25
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-01 19:24:35 +0530

    update

--- tle/slidingwindow.cpp
+++ tle/slidingwindow.cpp
@@ -12,3 +12,19 @@ using namespace std;
     }
 
  }
+
+int optimizedSlidingWindow(vector<int>arr,int k){
+    int maxi=INT_MIN;
+    int sum=0;
+    for(int i=0;i<k;i++){
+        sum+=arr[i];
+    }
+    maxi=max(maxi,sum);
+    for(int i=k;i<n;i++){
+        int first=i-k;
+        sum=sum+arr[i];
+        sum=sum-arr[first];
+    }
+    maxi=max(maxi,sum);
+
+}
