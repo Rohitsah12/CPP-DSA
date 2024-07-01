e4b0d5067a31dffddbb06edbaac5e1fa57219ca6
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-01 20:47:04 +0530

    first negative number in a windiows

--- tle/slidingwindow.cpp
+++ tle/slidingwindow.cpp
@@ -28,3 +28,38 @@ int optimizedSlidingWindow(vector<int>arr,int k){
     maxi=max(maxi,sum);
 
 }
+//Store Negative elements of the first windows
+int frist_windows_negative(vector<int>arr,int k){
+    deque<int>dq;
+    for(int i=0;i<k;i++){
+        if(arr[i]<0){
+            dq.push_back(i);
+        }
+    }
+    //print the answer for the first windows
+    if(dq.empty()==true){
+        cout<<0<<" ";
+    }
+    else{
+        cout<<arr[dq.front()]<<" ";
+    }
+
+    //use another loop to silde window one at a time
+    for(int i=k;i<n;i++){
+        int add_index=i;
+        int remove_index=i-k;
+        if(dq.front()==remove_index){
+            dq.pop_front();
+        }
+        if(arr[add_index]<0){
+            dq.push_back(add_index);
+        }
+        //We are at a new window so print the answer
+         if(dq.empty()==true){
+        cout<<0<<" ";
+    }
+    else{
+        cout<<arr[dq.front()]<<" ";
+    }
+    }
+}
