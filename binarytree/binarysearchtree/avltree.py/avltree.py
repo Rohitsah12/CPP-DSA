3adce20ccfdd38b94f0a46a59d1fdaf1765631bb
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-18 11:54:11 +0530

    update

--- binarytree/binarysearchtree/avltree.py/avltree.py
+++ binarytree/binarysearchtree/avltree.py/avltree.py
@@ -24,4 +24,45 @@ class AvlTree:
         else:
             return self.__contains(node.right,target)
         
+    def insert(self,value):
+        if value==None:
+            return False
+        if self.find(value):
+            return False
+        else:
+            self.__root=self.__insert(self.__root,value)
+            self.__nodesCount+=1
+            return True
         
+    def __insert(self,node,target):
+        if node is None:
+            return Node(target)
+        if target > node.value:
+            node.right=self.__insert(node.right,target)
+        else:
+            node.left=self.__insert(node.left,target)
+        
+        self.__update(node)
+        return self.__balance(node)
+    
+    def __update(self,node):
+        leftHeight,rightHeight=-1,-1
+        if node.left is not None:
+            leftHeight=node.left.height
+         
+        if node.right is not None:
+            rightHeight=node.right.height
+
+        node.height=1+max(leftHeight,rightHeight)
+        node.balanceFactor=rightHeight-leftHeight
+        
+    def __balance(self,node):
+        if node.balanceFactor == +2:
+            if node.right.balanceFactor>0:
+                return self.__rightRightCase(node)
+            else:
+                return self.__rightLeftCase(node)
+        
+        elif node.balanceFactor == -2:
+            if node.left.balanceFactor<0:
+                return self.__leftLeftCase
